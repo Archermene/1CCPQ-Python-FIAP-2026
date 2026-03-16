b9cce522e3e2c00637605e1c37a30050b1775fb5
Archermene <arturpereira360@gmail.com>
2026-03-16 20:45:53 -0300

    Aula 2 variáveis

---
+++ aula02-variaveis-operadores/Desafio 2.py
@@ -0,0 +1,7 @@
+print("Boa tarde, deixa eu descobrir sua data de nascimento")
+
+dia = input ("Digite o dia: ")
+mês = input("Digite o mês: ")
+ano = input("Digite o ano: ")
+
+print("Suas informações do seu nascimento são:", dia, mês, ano)
---
+++ aula02-variaveis-operadores/Desafio 3.py
@@ -0,0 +1,6 @@
+Numero1 = int(input("Digite um número: "))
+Numero2 = int(input("Digite um outro número ou o mesmo: "))
+soma =  Numero1 + Numero2
+
+
+print("A soma dos números (juntos apenas) é: ", soma )
